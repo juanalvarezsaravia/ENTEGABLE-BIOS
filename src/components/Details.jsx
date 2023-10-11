fc0efdb69a1a27b16defaf7b251dbc7d5bbd85f5
nazareno <naza21200021@gmail.com>
2023-10-11 14:29:04 -0300

    entregable

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -31,7 +31,7 @@ const Details = () => {
 
   return (
     <div className='Details'>
-      <h2>Detalles</h2>
+      <h2>DETALLES</h2>
       <h3>{repoDetails.name}</h3>
       <p>{repoDetails.description}</p>
       <p>Estrellas: {repoDetails.stargazers_count}</p>
