9f0bfc311d12d425e7d48413a292d1a8d3ee21e6
Juan Alvarez <juanalvarezsaravia@gmail.com>
2023-10-22 15:44:11 -0300

    gracias

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,12 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { SearchContext } from '../SearchContext';
 import Button from './Button';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 
 const Results = () => {
     const { results } = useContext(SearchContext);
     const navigate = useNavigate();
-
+    const [page, setPage] = useState(1);
+    const itemsPerPage = 10;
+    const paginatedResults = results.slice((page - 1) * itemsPerPage, page * itemsPerPage);
 
     if (!results || results.length === 0) {
         return <div>No results found.</div>;
@@ -15,11 +20,16 @@ const Results = () => {
     const goBackToSearch = () => {
         navigate('/');
     };
+
+    const handlePageChange = (event, value) => {
+        setPage(value);
+    };
+
     return (
         <div className='Results'>
             <h2>Search Results</h2>
             <ul>
-                {results.map((result) => (
+                {paginatedResults.map((result) => (
                     <li key={result.id}>
                         <Link to={`/details/${result.id}`}>{result.name}</Link>
                     </li>
@@ -27,10 +37,16 @@ const Results = () => {
             </ul>
             <Button onClick={goBackToSearch}> Back to Search</Button>
 
-
+            <Stack spacing={2}>
+                <Typography>Page: {page}</Typography>
+                <Pagination
+                    count={Math.ceil(results.length / itemsPerPage)}
+                    page={page}
+                    onChange={handlePageChange}
+                />
+            </Stack>
         </div>
     );
 }
 
 export default Results;
-
