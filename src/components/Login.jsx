8565f0d5b18359ba7bd9019e2312734813297fc9
Juan Alvarez <juanalvarezsaravia@gmail.com>
2023-10-22 17:03:45 -0300

    exelente

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
-import { useNavigate } from 'react-router-dom';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import Button from './Button';
+import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
+function Login() {
+    const [showPassword, setShowPassword] = useState(false);
     const navigate = useNavigate();
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
 
     const goBackToSearch = () => {
         navigate('/');
     };
 
-
     return (
-
         <div className='Login'>
             <h2>Acceso</h2>
             <form action="">
@@ -21,17 +33,33 @@ const Login = () => {
                     type='email'
                     size="small"
                 />
-                <TextField
-                    label="Contraseña"
-                    type='password'
-                    size="small"
-                />
+                <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-password"
+                        type={showPassword ? 'text' : 'password'}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                    color="inherit"
+                                    sx={{ color: 'white' }}
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="Password"
+                    />
+                </FormControl>
                 <Button type="submit">Enviar</Button>
             </form>
             <Button onClick={goBackToSearch}> Back to Search</Button>
         </div>
-
-    )
+    );
 }
 
 export default Login;
