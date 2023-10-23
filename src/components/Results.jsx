adec25afd310efe2ed48daaff9bae955442e8303
nazareno <naza21200021@gmail.com>
2023-10-23 14:06:56 -0300

    entregable

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,33 +3,32 @@ import { Link, useNavigate } from 'react-router-dom';
 import { SearchContext } from '../SearchContext';
 
 const Results = () => {
-    const { results } = useContext(SearchContext);
-    const navigate = useNavigate();
+  const { results } = useContext(SearchContext);
+  const navigate = useNavigate();
 
 
-    if (!results || results.length === 0) {
-        return <div>No results found.</div>;
-    }
+  if (!results || results.length === 0) {
+    return <div>No se encontraron resultados</div>;
+  }
 
-    const goBackToSearch = () => {
-        navigate('/');
-    };
-    return (
-        <div className='Results'>
-            <h2>Search Results</h2>
-            <ul>
-                {results.map((result) => (
-                    <li key={result.id}>
-                        <Link to={`/details/${result.id}`}>{result.name}</Link>
-                    </li>
-                ))}
-            </ul>
-            <button onClick={goBackToSearch}>Back to Search</button>
+  const goBackToSearch = () => {
+    navigate('/');
+  };
+  return (
+    <div className='Results'>
+      <h2>RESULTADOS DE BUSQUEDA</h2>
+      <ul>
+        {results.map((result) => (
+          <li key={result.id}>
+            <Link to={`/details/${result.id}`}>{result.name}</Link>
+          </li>
+        ))}
+      </ul>
+      <button onClick={goBackToSearch}>Volver</button>
 
 
-        </div>
-    );
+    </div>
+  );
 }
 
 export default Results;
-
